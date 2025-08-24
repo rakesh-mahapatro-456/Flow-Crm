07766329bc418c2329bd8a6e6e631f5eb293d182
Rakesh Mahapatro <rakeshmahapatro85@gmail.com>
2025-08-24 22:22:04 +0530

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express(); 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true
 };
 app.use( cors(corsOptions) );
